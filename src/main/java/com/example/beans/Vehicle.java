0fb76b1c495f7697671aa70de135972642f410f5
ayman <a.m_ayman9@outlook.com>
2025-09-09 02:10:27 +0300

    post construct annotation example

--- src/main/java/com/example/beans/Vehicle.java
+++ src/main/java/com/example/beans/Vehicle.java
@@ -1,5 +1,6 @@
 package com.example.beans;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,5 +18,10 @@ public class Vehicle {
 
     public void printHello(){
         System.out.println("Printing Hello from Component vehicle Bean");
+    }
+
+    @PostConstruct
+    public void initialize(){
+        this.name = "Honda";
     }
 }
